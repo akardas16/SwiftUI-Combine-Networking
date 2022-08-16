382c0c5c545d4ac0e28cc9429c65900523511f72
akardas16 <krdsabdllh53@gmail.com>
2022-08-16 15:23:18 +0300

    Add files via upload

---
+++ NetworkingLayer/QueryItems.swift
@@ -0,0 +1,31 @@
+import Foundation
+//Some example queries
+enum QueryItems{
+    case login(email:String,password:String)
+    case signup(email:String,password:String)
+    case saveBio(bio:String)
+    case createUser(name:String,job:String)
+    case empty
+    
+    var queries:[URLQueryItem] {
+        switch self {
+        case .login(let email, let password):
+            return [URLQueryItem(name: "email", value: email),
+                    URLQueryItem(name: "password", value: password)]
+        case .signup(let email, let password):
+            return [URLQueryItem(name: "email", value: email),
+                    URLQueryItem(name: "password", value: password)]
+        case .saveBio(let bio):
+            return [URLQueryItem(name: "userBio", value: bio)]
+            
+        case .empty:
+            return []
+        case .createUser(name: let name, job: let job):
+            return [URLQueryItem(name: "name", value: name),
+            URLQueryItem(name: "job", value: job)]
+        }
+        
+    }
+    
+}
+
